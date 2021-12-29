abd902fcb1df19b4545a35d7720f10664e9d6288
Yishai Berg <yishai.berg@dreamed.ai>
2021-12-30 00:57:37 +0200

    Add a failing test for onChange event in select element

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+  it('does not fire an onChange event when there is no change in a select element', () => {
+    cy.visit('https://yishaiberg.github.io/cypress-onchange-bug/');
+    cy.get('.ice-cream').select('Chocolate');
+    cy.get('.result').should('have.text', 'Number of times onChange event fired: 1')
+    // Select the option that is already selected - onChange event should not fire.
+    cy.get('.ice-cream').select('Chocolate');
+    cy.get('.result').should('have.text', 'Number of times onChange event fired: 1')
   })
 })
